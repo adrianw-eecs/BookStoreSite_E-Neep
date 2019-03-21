4cddc21355bb687e6b4814e8498e18aaee25bd77
Adr ADR <adrianw@my.yorku.ca>
2019-03-21 14:59:49 -0400

    Created all the Beans for the Address, Book, PO, POItem, and visitevent.

--- src/bean/POBean.java
+++ src/bean/POBean.java
@@ -1,5 +1,69 @@
 package bean;
 
 public class POBean {
+	
+	private int id;
+	private String fname;
+	private String lname;
+	private String status;
+	private int address;
+	
+	public POBean(int id, String fname, String lname, String status, int address) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.lname = lname;
+		this.status = status;
+		this.address = address;
+	}
+
+	public POBean() {
+		this.id = 0;
+		this.fname = "";
+		this.lname = "";
+		this.status = "";
+		this.address = 0;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getAddress() {
+		return address;
+	}
+
+	public void setAddress(int address) {
+		this.address = address;
+	}
+	
 
 }
--- src/bean/POItemBean.java
+++ src/bean/POItemBean.java
@@ -2,4 +2,47 @@ package bean;
 
 public class POItemBean {
 
+	private int id;
+	private String bid;
+	private double price;
+	
+	public POItemBean(int id, String bid, double price) {
+		super();
+		this.id = id;
+		this.bid = bid;
+		this.price = price;
+	}
+	
+	public POItemBean() {
+		this.id = 0;
+		this.bid = "";
+		this.price = 0.00;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBid() {
+		return bid;
+	}
+
+	public void setBid(String bid) {
+		this.bid = bid;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
 }
