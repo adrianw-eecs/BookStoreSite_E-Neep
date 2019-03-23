374232535c20bc62e92fc1fe00455667ddb9b990
Adr ADR <adrianw@my.yorku.ca>
2019-03-23 17:27:34 -0400

    Added methods for searching books

--- src/dao/BookDAO.java
+++ src/dao/BookDAO.java
@@ -16,9 +16,8 @@ import org.apache.tomcat.util.buf.StringUtils;
 import bean.BookBean;
 
 public class BookDAO {
-	
+
 	private DataSource ds;
-	
 
 	public BookDAO() throws ClassNotFoundException {
 		try {
@@ -28,25 +27,26 @@ public class BookDAO {
 		}
 
 	}
-	
-	
-	public ArrayList<BookBean> retrieveAnyBookOrBooks(String bid, String category, ArrayList<String> bids) throws SQLException {
-		//String query = "select * from students where surname like ? and credit_taken >= ?";
+
+	public ArrayList<BookBean> retrieveAnyBookOrBooks(String bid, String category, ArrayList<String> bids)
+			throws SQLException {
+		// String query = "select * from students where surname like ? and credit_taken
+		// >= ?";
 		String query = "Select * FROM BOOK ";
-		Connection con = this.ds.getConnection(); 
+		Connection con = this.ds.getConnection();
 		String queryValueString = "";
-		if(!bid.equals("")) {
+		if (!bid.equals("")) {
 			query = query + "WHERE BID = ?";
 			queryValueString = bid;
-		} else if(!category.equals("")) {
+		} else if (!category.equals("")) {
 			query = query + "WHERE CATEGORY = ?";
 			queryValueString = category;
-		} else if(!bids.isEmpty()) {
+		} else if (!bids.isEmpty()) {
 			query = query + "WHERE BID IN (";
-			for(String temp : bids) {
+			for (String temp : bids) {
 				query = query + "?,";
 			}
-			query = query.substring(0, query.length() -1);
+			query = query.substring(0, query.length() - 1);
 			query = query + ")";
 		} else {
 			System.out.println("All Parameters passed to the BookDAO are empty");
@@ -54,18 +54,18 @@ public class BookDAO {
 
 //		Map<String, AddressBean> rv = new HashMap<String, AddressBean>();
 		ArrayList<BookBean> arraylist = new ArrayList<BookBean>();
-		
+
 		PreparedStatement sanatizedQuery = con.prepareStatement(query);
 		try {
-			if(bids != null) {
-				for(int i = 0; i < bids.size(); i++) {
+			if (bids != null) {
+				for (int i = 0; i < bids.size(); i++) {
 					sanatizedQuery.setString(i + 1, bids.get(i));
 				}
 			} else {
 				sanatizedQuery.setString(1, queryValueString);
 			}
 //			sanatizedQuery.setInt(1, Integer.parseInt(id));
-			
+
 			ResultSet r = sanatizedQuery.executeQuery();
 			while (r.next()) {
 				int result_price = r.getInt("PRICE");
@@ -73,20 +73,50 @@ public class BookDAO {
 				String result_title = r.getString("TITLE");
 				String result_category = r.getString("CATEGORY");
 
-
-				//CHANGE Query for the current credits
+				// CHANGE Query for the current credits
 				BookBean book = new BookBean(result_bid, result_title, result_price, result_category);
 				arraylist.add(book);
 			}
-		} catch(SQLException e){
+		} catch (SQLException e) {
 			System.out.println("Query parsing and results in BOOKDAO failed");
 			throw new SQLException("");
 		}
-		
-		
+
 		sanatizedQuery.close();
 		con.close();
 		return arraylist;
 	}
-	
+
+	public ArrayList<BookBean> findBooks(String searchStr) throws SQLException {
+		// String query = "select * from students where surname like ? and credit_taken
+		// >= ?";
+		String query = "Select * FROM BOOK WHERE TITLE like ?";
+		Connection con = this.ds.getConnection();
+		searchStr = "%" + searchStr + "%";
+		ArrayList<BookBean> arraylist = new ArrayList<BookBean>();
+
+		PreparedStatement sanatizedQuery = con.prepareStatement(query);
+		try {
+			sanatizedQuery.setString(1, searchStr);
+			ResultSet r = sanatizedQuery.executeQuery();
+			while (r.next()) {
+				int result_price = r.getInt("PRICE");
+				String result_bid = r.getString("BID");
+				String result_title = r.getString("TITLE");
+				String result_category = r.getString("CATEGORY");
+
+				// CHANGE Query for the current credits
+				BookBean book = new BookBean(result_bid, result_title, result_price, result_category);
+				arraylist.add(book);
+			}
+		} catch (SQLException e) {
+			System.out.println("Query parsing and results in BOOKDAO failed");
+			throw new SQLException("");
+		}
+
+		sanatizedQuery.close();
+		con.close();
+		return arraylist;
+	}
+
 }
