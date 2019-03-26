05a52ea8144629d00804c104b26cd2a5853acf8b
Adham El-Shafie <xtremeatp@hotmail.com>
2019-03-26 17:03:44 -0400

    Finished layout of Payment Page
    - have to implement information getters/setters with servlet on payment
    - have to beautify everything still

--- WebContent/res/mc.js
+++ WebContent/res/mc.js
@@ -241,3 +241,92 @@ function calculateTotal(func, cost){
 	target.innerHTML = "$" + Math.max(roundedPrice, 0);
 	
 }
+
+function login(address){
+	document.getElementById("pTable").style.display = "";
+	document.getElementById("infoTable").style.display = "none";
+	document.getElementById("loginNow").hidden = false;
+	document.getElementById("createNow").hidden = true;
+}
+
+function create(address){
+	document.getElementById("pTable").style.display = "";
+	document.getElementById("infoTable").style.display = "none";
+	document.getElementById("loginNow").hidden = true;
+	document.getElementById("createNow").hidden = false;
+}
+
+function login2(address){
+	//validate();
+	document.getElementById("infoTable").style.display = "";
+	hideRest();
+	hideCreateDisplay();
+	document.getElementById("fnameLabel").innerHTML = "Steve";
+	document.getElementById("lnameLabel").innerHTML = "Irwin";
+	document.getElementById("streetNameLabel").innerHTML = "42 Wallaby Way";
+	document.getElementById("provLabel").innerHTML = "Sydney";
+	document.getElementById("ctLabel").innerHTML = "Australia";
+	document.getElementById("zipLabel").innerHTML = "M3J 3S8";
+	document.getElementById("phnLabel").innerHTML = "416-123-4567";
+	
+	document.getElementById("fnameLabel").hidden = false;
+	document.getElementById("lnameLabel").hidden = false;
+	document.getElementById("streetNameLabel").hidden = false;
+	document.getElementById("provLabel").hidden = false;
+	document.getElementById("ctLabel").hidden = false;
+	document.getElementById("zipLabel").hidden = false;
+	document.getElementById("phnLabel").hidden = false;
+	
+	document.getElementById("confirmAndPay").hidden = false;
+}
+
+function create2(address){
+	//validate();
+	document.getElementById("infoTable").style.display = "";
+	hideRest();
+	hideLoginDisplay();
+	document.getElementById("fnameInp").hidden = false;
+	document.getElementById("lnameInp").hidden = false;
+	document.getElementById("streetNameInp").hidden = false;
+	document.getElementById("provInp").hidden = false;
+	document.getElementById("ctInp").hidden = false;
+	document.getElementById("zipInp").hidden = false;
+	document.getElementById("phnInp").hidden = false;
+	
+	document.getElementById("confirmAndPay").hidden = false;
+	
+}
+
+function hideLoginDisplay(){
+	document.getElementById("fnameLabel").hidden = true;
+	document.getElementById("lnameLabel").hidden = true;
+	document.getElementById("streetNameLabel").hidden = true;
+	document.getElementById("provLabel").hidden = true;
+	document.getElementById("ctLabel").hidden = true;
+	document.getElementById("zipLabel").hidden = true;
+	document.getElementById("phnLabel").hidden = true;
+}
+
+function hideCreateDisplay(){
+	document.getElementById("fnameInp").hidden = true;
+	document.getElementById("lnameInp").hidden = true;
+	document.getElementById("streetNameInp").hidden = true;
+	document.getElementById("provInp").hidden = true;
+	document.getElementById("ctInp").hidden = true;
+	document.getElementById("zipInp").hidden = true;
+	document.getElementById("phnInp").hidden = true;	
+}
+
+function showCreditCard(address){
+	//validate();
+	document.getElementById("credit").hidden = false;
+	document.getElementById("creditNum").hidden = false;
+	document.getElementById("confirmOrder").hidden = false;
+}
+
+function hideRest(){
+	document.getElementById("pTable").style.display = "none";
+	document.getElementById("credit").hidden = true;
+	document.getElementById("creditNum").hidden = true;
+	document.getElementById("confirmOrder").hidden = true;
+}
